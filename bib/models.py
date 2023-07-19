78810c67f66befbe9b7cc6ad046eda3a3472a6b6
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-07-19 14:09:21 -0400

    Add additional fields

--- bib/models.py
+++ bib/models.py
@@ -38,43 +38,133 @@ class ZotItem(models.Model):
     )
     zot_creator = models.TextField(
         blank=True, verbose_name="creators",
-        help_text="Stores all information from zoteros 'creators' field."
+        help_text="Stores all information from Zotero's 'creators' field."
     )
     zot_date = models.TextField(
         blank=True, verbose_name="date",
-        help_text="Stores all information from zoteros 'date' field."
+        help_text="Stores all information from Zotero's 'date' field."
     )
     zot_item_type = models.TextField(
         blank=True, verbose_name="itemType",
-        help_text="Stores all information from zoteros 'itemType' field."
+        help_text="Stores all information from Zotero's 'itemType' field."
     )
     zot_title = models.TextField(
         blank=True, verbose_name="title",
-        help_text="Stores all information from zoteros 'title' field."
+        help_text="Stores all information from Zotero's 'title' field."
     )
+    # TODO was this removed in API v3?
     zot_pub_title = models.TextField(
         blank=True, verbose_name="publicationTitle",
         help_text="Stores all information from zoteros 'publicationTitle' field."
     )
+    short_title = models.TextField(
+        blank=True, null=True, verbose_name="short title",
+        help_text="Stores all information from Zotero's 'shortTitle' field."
+    )
+    abstract_note = models.TextField(
+        blank=True, null=True, verbose_name="abstract note",
+        help_text="Stores all information from Zotero's 'abstractNote' field."
+    )
+    series = models.TextField(
+        blank=True, null=True, verbose_name="series",
+        help_text="Stores all information from Zotero's 'series' field."
+    )
+    series_number = models.TextField(
+        blank=True, null=True, verbose_name="series number",
+        help_text="Stores all information from Zotero's 'seriesNumber' field."
+    )
+    volume = models.TextField(
+        blank=True, null=True, verbose_name="volume",
+        help_text="Stores all information from Zotero's 'volume' field."
+    )
+    number_of_volumes = models.TextField(
+        blank=True, null=True, verbose_name="number of volumes",
+        help_text="Stores all information from Zotero's 'numberOfVolumes' field."
+    )
+    edition = models.TextField(
+        blank=True, null=True, verbose_name="edition",
+        help_text="Stores all information from Zotero's 'edition' field."
+    )
+    place = models.TextField(
+        blank=True, null=True, verbose_name="place",
+        help_text="Stores all information from Zotero's 'place' field."
+    )
+    publisher = models.TextField(
+        blank=True, null=True, verbose_name="publisher",
+        help_text="Stores all information from Zotero's 'publisher' field."
+    )
+    language = models.TextField(
+        blank=True, null=True, verbose_name="language",
+        help_text="Stores all information from Zotero's 'language' field."
+    )
+    isbn = models.TextField(
+        blank=True, null=True, verbose_name="isbn",
+        help_text="Stores all information from Zotero's 'ISBN' field."
+    )
+    url = models.TextField(
+        blank=True, null=True, verbose_name="url",
+        help_text="Stores all information from Zotero's 'url' field."
+    )
+    access_date = models.TextField(
+        blank=True, null=True, verbose_name="access date",
+        help_text="Stores all information from Zotero's 'accessDate' field."
+    )
+    archive = models.TextField(
+        blank=True, null=True, verbose_name="archive",
+        help_text="Stores all information from Zotero's 'archive' field."
+    )
+    archive_location = models.TextField(
+        blank=True, null=True, verbose_name="archive location",
+        help_text="Stores all information from Zotero's 'archiveLocation' field."
+    )
+    library_catalog = models.TextField(
+        blank=True, null=True, verbose_name="library catalog",
+        help_text="Stores all information from Zotero's 'libraryCatalog' field."
+    )
+    call_number = models.TextField(
+        blank=True, null=True, verbose_name="call number",
+        help_text="Stores all information from Zotero's 'callNumber' field."
+    )
+    rights = models.TextField(
+        blank=True, null=True, verbose_name="rights",
+        help_text="Stores all information from Zotero's 'rights' field."
+    )
+    extra = models.TextField(
+        blank=True, null=True, verbose_name="extra",
+        help_text="Stores all information from Zotero's 'extra' field."
+    )
+    # tags and collections are arrays, like creator, not using ArrayField to keep sqllite compatability
+    tags = models.TextField(
+        blank=True, null=True, verbose_name="tag",
+        help_text="Stores all information from Zotero's 'tags' field."
+    )
+    collections= models.TextField(
+        blank=True, null=True, verbose_name="collection",
+        help_text="Stores all information from Zotero's 'collections' field."
+    )
+    date_added = models.DateTimeField(
+        blank=True, null=True, verbose_name="dateAdded",
+        help_text="Stores all information from Zotero's 'dateAdded' field."
+    )
     date_modified = models.DateTimeField(
         blank=True, null=True, verbose_name="dateModified",
-        help_text="Stores all information from zoteros 'publicationTitle' field."
+        help_text="Stores all information from Zotero's 'dateModified' field."
     )
     zot_pages = models.TextField(
         blank=True, verbose_name="pages",
-        help_text="Stores all information from zoteros 'pages' field."
+        help_text="Stores all information from Zotero's 'numPages' field."
     )
     zot_version = models.IntegerField(
         blank=True, null=True, verbose_name="version",
-        help_text="Stores all information from zoteros 'pages' field."
+        help_text="Stores all information from Zotero's 'version' field."
     )
     zot_html_link = models.CharField(
         blank=True, verbose_name="selflink html", max_length=500,
-        help_text="Stores all information from zoteros 'selflink' field."
+        help_text="Stores all information from Zotero's 'selflink' field."
     )
     zot_api_link = models.CharField(
         blank=True, verbose_name="selflink api", max_length=500,
-        help_text="Stores all information from zoteros self api link field."
+        help_text="Stores all information from Zotero's self api link field."
     )
     zot_bibtex = models.TextField(
         blank=True, verbose_name="bibtex",
