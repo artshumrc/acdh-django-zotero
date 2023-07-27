9fe6876122fd86338cdfdd6abb4d2f3e7db85f5d
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-07-27 18:14:11 -0400

    Change url for re_path
    Deprecated since Django 3, removed in Django 4

--- bib/urls.py
+++ bib/urls.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 from . import dal_views
 
 app_name = 'bib'
 
 urlpatterns = [
-    url(r'^synczotero/$', views.sync_zotero, name="synczotero"),
-    url(r'^synczotero/update$', views.update_zotitems, name="synczotero_update"),
-    url(
+    re_path(r'^synczotero/$', views.sync_zotero, name="synczotero"),
+    re_path(r'^synczotero/update$', views.update_zotitems, name="synczotero_update"),
+    re_path(
         r'^zotitem-autocomplete/$', dal_views.ZotItemAC.as_view(),
         name='zotitem-autocomplete',
     ),
