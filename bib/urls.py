788e3b315b105d5b294b89f347ccc15fadd174ce
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-08-10 12:59:28 -0400

    Try using path rather than re_path

--- bib/urls.py
+++ bib/urls.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.urls import re_path
+from django.urls import path
 from . import views
 from . import dal_views
 
 app_name = 'bib'
 
 urlpatterns = [
-    re_path(r'^synczotero/$', views.sync_zotero, name="synczotero"),
-    re_path(r'^synczotero/update$', views.update_zotitems, name="synczotero_update"),
-    re_path(
-        r'^zotitem-autocomplete/$', dal_views.ZotItemAC.as_view(),
+    path('synczotero/', views.sync_zotero, name="synczotero"),
+    path('synczotero/update/', views.update_zotitems, name="synczotero_update"),
+    path(
+        'zotitem-autocomplete/',
+        dal_views.ZotItemAC.as_view(),
         name='zotitem-autocomplete',
     ),
 ]
